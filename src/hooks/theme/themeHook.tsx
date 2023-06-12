8e83aaccfe0f949f87cbbf37d8be24c60865a44b
Rahul Vig <rahulvig315@gmail.com>
2023-06-12 13:37:31 -0400

    feat(theming): create useTheme hook

--- src/hooks/theme/themeHook.tsx
+++ src/hooks/theme/themeHook.tsx
@@ -2,21 +2,21 @@
 import { Themes, getDarkModeQuery } from "@/utilities/theme";
 import { useEffect, useState } from "react";
 
-type Props = {}
-
 const useTheme = (): { theme: Themes } => {
-    const [theme, setTheme] = useState(() => getDarkModeQuery()?.matches ? Themes.DARK : Themes.LIGHT);
-
+    const [theme, setTheme] = useState<Themes | null>(null);
     useEffect(() => {
+        if (global.window && !theme) {
+            setTheme((() => !!getDarkModeQuery()?.matches ? Themes.DARK : Themes.LIGHT))
+        }
         let darkModeMediaQuery = getDarkModeQuery();
         darkModeMediaQuery.addEventListener('change', (e) => setTheme(e.matches ? Themes.DARK : Themes.LIGHT));
         return () => (darkModeMediaQuery.removeEventListener('change', (e) => { }));
-    }, [])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return {
-        theme
+        theme: (theme as Themes)
     }
-
 }
 
 export default useTheme;
