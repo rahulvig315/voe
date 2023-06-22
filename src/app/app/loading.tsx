81ce6f1b2db7de918016ae3a9be751add852afb9
Rahul Vig <rahulvig315@gmail.com>
2023-06-22 14:08:27 -0400

    feat(app): basic loading

---
+++ src/app/app/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+import { useUser } from "@auth0/nextjs-auth0/client";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+
+
+export default function Loading() {
+
+
+  return (
+    <div>Loading...</div>
+  )
+}
