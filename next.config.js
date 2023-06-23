aba5570877462da919caad0a36495742d867233d
Rahul Vig <rahulvig315@gmail.com>
2023-06-23 14:11:26 -0400

    feat(nextConfig): allow images

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@ const nextConfig = {
             }
         ];
     },
+    images: {
+        remotePatterns: [
+          {
+            protocol: "https",
+            hostname: "**",
+          },
+          {
+            protocol: "http",
+            hostname: "**",
+          },
+        ],
+      },
 }
 
 module.exports = nextConfig
